af791b91a4c61d1cae9779cb7ec1463713d9e0af
Alberta Williams <alberta04@gmail.com>
2018-04-23 01:16:34 -0500

    creates a node server

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.set('port', process.env.PORT || 5000);
+app.use(express.static(path.join(__dirname, '../build')));
+
+app.listen(app.get('port'), function() {
+	console.log('Express server listening on port', app.get('port'));
+});
