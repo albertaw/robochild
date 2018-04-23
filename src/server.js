4bf8d85b49a19f68373bdf5008c3669cf1c34ffe
Alberta Williams <alberta04@gmail.com>
2018-04-23 01:26:18 -0500

    moves build dir into src dir

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 app.set('port', process.env.PORT || 5000);
-app.use(express.static(path.join(__dirname, '../build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.listen(app.get('port'), function() {
 	console.log('Express server listening on port', app.get('port'));
